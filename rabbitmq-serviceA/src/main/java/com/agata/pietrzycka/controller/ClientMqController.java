846eba6db944e4f1fdf77fe7937711837211d4db
heymarc3lina <ananaS212>
2023-07-17 20:21:11 +0200

    add metrics

--- rabbitmq-serviceA/src/main/java/com/agata/pietrzycka/controller/ClientMqController.java
+++ rabbitmq-serviceA/src/main/java/com/agata/pietrzycka/controller/ClientMqController.java
@@ -10,11 +10,22 @@ import org.springframework.stereotype.Component;
 public class ClientMqController {
 
     private final ServiceAService serviceAService;
+    private final Timer messageProcessingTimer;
+
+    public MessageListener(MeterRegistry registry) {
+        // Inicjalizacja Timera do monitorowania czasu przetwarzania wiadomości
+        messageProcessingTimer = Timer.builder("message.processing.time")
+                .description("Czas przetwarzania wiadomości z RabbitMQ")
+                .publishPercentiles(0.99) // Dodanie 99 percentyla do timera
+                .register(registry);
+    }
 
     @RabbitListener(queues = "serviceA")
     public void listener(String s){
+        Timer.Sample sample = Timer.start();
         int loopNumber = Integer.parseInt(s);
         serviceAService.calculateAndGive10RandomSHA(loopNumber);
         System.out.println(s);
+        sample.stop(messageProcessingTimer);
     }
 }
