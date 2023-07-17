e45420bb030cece1ed46166c79afa9dacc666646
Agata Pietrzycka <pietrzycka.agata@gmail.com>
2023-07-17 22:19:35 +0200

    metrics

--- rabbitmq-serviceB/src/main/java/com/agata/pietrzycka/controller/ClientMqController.java
+++ rabbitmq-serviceB/src/main/java/com/agata/pietrzycka/controller/ClientMqController.java
@@ -10,21 +10,11 @@ import org.springframework.stereotype.Component;
 public class ClientMqController {
 
     private final ServiceBService serviceBService;
-    private final Timer messageProcessingTimer;
 
-    public MessageListener(MeterRegistry registry) {
-        // Inicjalizacja Timera do monitorowania czasu przetwarzania wiadomości
-        messageProcessingTimer = Timer.builder("message.processing.time")
-                .description("Czas przetwarzania wiadomości z RabbitMQ")
-                .publishPercentiles(0.99) // Dodanie 99 percentyla do timera
-                .register(registry);
-    }
     @RabbitListener(queues = "serviceB")
     public void listener(String s){
-        Timer.Sample sample = Timer.start();
         int loopNumber = Integer.parseInt(s);
         serviceBService.calculateAndGive10RandomSHA(loopNumber);
         System.out.println(s);
-        sample.stop(messageProcessingTimer);
     }
 }
