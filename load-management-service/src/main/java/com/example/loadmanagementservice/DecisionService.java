4ce86eb5386aea89713189a99a156a39794f70bc
Agata Pietrzycka <pietrzycka.agata@gmail.com>
2023-08-12 00:32:03 +0200

    improvement

--- load-management-service/src/main/java/com/example/loadmanagementservice/DecisionService.java
+++ load-management-service/src/main/java/com/example/loadmanagementservice/DecisionService.java
@@ -2,6 +2,8 @@ package com.example.loadmanagementservice;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -24,30 +26,49 @@ public class DecisionService {
         PrometheusMetrics prometheusMetrics = prometheusService.getMetrics();
         Optional<PrometheusMetricResult> prometheusMetricResultServiceA = prometheusMetrics.getData().getResult().stream().filter(e-> e.getMetric().getJob().equals("serviceA_service")).findFirst();
         Optional<PrometheusMetricResult> prometheusMetricResultServiceB = prometheusMetrics.getData().getResult().stream().filter(e-> e.getMetric().getJob().equals("serviceB_service")).findFirst();
-        if(prometheusMetricResultServiceA.isPresent() && prometheusMetricResultServiceB.isPresent()){
+        PrometheusMetrics prometheusMetricsSum = prometheusService.getMetricsSum();
+        Optional<PrometheusMetricResult> prometheusMetricResultServiceASum = prometheusMetricsSum.getData().getResult().stream().filter(e-> e.getMetric().getJob().equals("serviceA_service")).findFirst();
+        Optional<PrometheusMetricResult> prometheusMetricResultServiceBSum = prometheusMetricsSum.getData().getResult().stream().filter(e-> e.getMetric().getJob().equals("serviceB_service")).findFirst();
+        if(prometheusMetricResultServiceA.isPresent() && prometheusMetricResultServiceB.isPresent() && prometheusMetricResultServiceASum.isPresent() && prometheusMetricResultServiceBSum.isPresent()){
             double timeServiceA = Double.valueOf(prometheusMetricResultServiceA.get().getValues().get(0).get(1));
             double timeServiceB = Double.valueOf(prometheusMetricResultServiceB.get().getValues().get(0).get(1));
-            if(timeServiceB > timeServiceA){
-                if(dockerClientService.canServiceKill(IMAGE_A_NAME)){
-                    newNumberServiceB++;
-                    dockerClientService.killInstance(IMAGE_A_NAME);
-                    dockerClientService.createInstance(newNumberServiceB, SERVICE_B_NAME, IMAGE_B_NAME);
-                    System.out.println("ADD B");
+            double timeServiceASum = Double.valueOf(prometheusMetricResultServiceASum.get().getValues().get(0).get(1));
+            double timeServiceBSum = Double.valueOf(prometheusMetricResultServiceBSum.get().getValues().get(0).get(1));
+            if(timeServiceB < timeServiceA && timeServiceBSum > timeServiceASum){
+               addB();
+            } else if (timeServiceB > timeServiceA && timeServiceBSum < timeServiceASum){
+                addA();
+            } else {
+                System.out.println("Nothing to do, however...");
+                if (timeServiceB > timeServiceA) {
+                    addB();
                 } else {
-                    System.out.println("Can't kill serviceA, because there left only 1!");
-                }
-            }
-            if(timeServiceB < timeServiceA){
-                if(dockerClientService.canServiceKill(IMAGE_B_NAME)){
-                    newNumberServiceA++;
-                    dockerClientService.killInstance(IMAGE_B_NAME);
-                    dockerClientService.createInstance(newNumberServiceA, SERVICE_A_NAME, IMAGE_A_NAME);
-                    System.out.println("ADD A");
-                } else {
-                    System.out.println("Can't kill serviceB, because there left only 1!");
+                    addA();
                 }
             }
         }
         dockerClientService.cleanUp();
+    }
+
+    private void addB(){
+        if(dockerClientService.canServiceKill(IMAGE_A_NAME)){
+            newNumberServiceB++;
+            dockerClientService.killInstance(IMAGE_A_NAME);
+            dockerClientService.createInstance(newNumberServiceB, SERVICE_B_NAME, IMAGE_B_NAME);
+            System.out.println(new Timestamp(new Date().getTime()) + " ADD B");
+        } else {
+            System.out.println(new Timestamp(new Date().getTime()) + " Can't kill serviceA, because there left only 1!");
+        }
+    }
+
+    private void addA(){
+        if(dockerClientService.canServiceKill(IMAGE_B_NAME)){
+            newNumberServiceA++;
+            dockerClientService.killInstance(IMAGE_B_NAME);
+            dockerClientService.createInstance(newNumberServiceA, SERVICE_A_NAME, IMAGE_A_NAME);
+            System.out.println(new Timestamp(new Date().getTime()) + " ADD A");
+        } else {
+            System.out.println(new Timestamp(new Date().getTime()) + " Can't kill serviceB, because there left only 1!");
+        }
     }
 }
--- load-management-service/src/main/java/com/example/loadmanagementservice/PrometheusService.java
+++ load-management-service/src/main/java/com/example/loadmanagementservice/PrometheusService.java
@@ -1,19 +1,15 @@
 package com.example.loadmanagementservice;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 @Service
 @NoArgsConstructor
 public class PrometheusService {
 
     private final String PROMETHEUS_API_URL = "/api/v1/query_range?query=spring_rabbitmq_listener_seconds_sum/spring_rabbitmq_listener_seconds_count";
+    private final String PROMETHEUS_API_URL_SUM = "/api/v1/query_range?query=spring_rabbitmq_listener_seconds_sum";
     private final String PROMETHEUS_API_BASE_URL = "http://localhost:9090";
 
     public PrometheusMetrics getMetrics() {
@@ -22,7 +18,14 @@ public class PrometheusService {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate
                 .getForObject(PROMETHEUS_API_BASE_URL + url, PrometheusMetrics.class);
-
     }
 
+    public PrometheusMetrics getMetricsSum() {
+        long timeStamp = System.currentTimeMillis() / 1000;
+        String url = PROMETHEUS_API_URL_SUM + "&start=" + timeStamp + "&end=" + timeStamp + "&step=1";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate
+                .getForObject(PROMETHEUS_API_BASE_URL + url, PrometheusMetrics.class);
+
+    }
 }
